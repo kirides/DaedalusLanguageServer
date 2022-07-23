b8a0c0f440dd2b6af4ee953011a783e9d8592c1d
Kirides <john.heckendorf@outlook.com>
2022-07-23 12:24:25 +0200

    update antlr build batch

--- antlr_parser/compile-parser-go.bat
+++ antlr_parser/compile-parser-go.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 PUSHD %~dp0
 REM Change this to antlr command line:
-SET ANTLR=java -jar "antlr-4.9.3-complete.jar"
+SET ANTLR=java -jar "antlr-4.10.1-complete.jar"
 %ANTLR% -o ../langserver/parser -Dlanguage=Go Daedalus.g4
 REM %ANTLR% -visitor -o ../langserver/parser -Dlanguage=Go Daedalus.g4
 popd
