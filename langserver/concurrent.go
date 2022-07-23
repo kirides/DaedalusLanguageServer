1bdbf8e262129cb06694cc0bbb4da5b9abddf87d
Kirides <john.heckendorf@outlook.com>
2022-07-23 09:39:32 +0200

    remove unused code

--- langserver/concurrent.go
+++ langserver/concurrent.go
@@ -2,24 +2,6 @@ package langserver
 
 import "sync"
 
-type concurrentMap[K comparable, V any] struct {
-	m sync.Map
-}
-
-func (c *concurrentMap[K, V]) Store(key K, value V) {
-	c.m.Store(key, value)
-}
-
-func (c *concurrentMap[K, V]) Load(key K) (V, bool) {
-	var def V
-	v, loaded := c.m.Load(key)
-
-	if !loaded {
-		return def, false
-	}
-	return v.(V), true
-}
-
 type concurrentSet[K comparable] struct {
 	m sync.Map
 }
